85123e9e48f71acc74558fc6bf20ec5f2606d123
Pepparmint <patrik.hallqvist@student.nbi-handelsakademin.se>
2023-11-13 14:45:17 +0100

    [DataRow]

--- FizzBuzz Test/KalkylatorTests.cs
+++ FizzBuzz Test/KalkylatorTests.cs
@@ -6,20 +6,24 @@ namespace FizzBuzz_Test
     [TestClass]
     public class KalkylatorTests
     {
+        [DataRow(15)]
+        [DataRow(3)]
+        [DataRow(5)]
+        [DataRow(1)]
         [TestMethod]
-        public void När_FizzBuzzKalkyl_EjImplementerad_Then_ShouldThrow_NotImplementedException()
+        public void När_FizzBuzzKalkyl_EjImplementerad_Then_ShouldThrow_NotImplementedException(int randomNummer)
         {
             // Arrange
-            int randomNummer = 1;
+
 
             // Act
             string result = Kalkylator.FizzBuzzKalkyl(randomNummer);
 
             // Assert
-            //Assert.AreEqual("Fizz", result);
-            //Assert.AreEqual("Buzz", result);
             Assert.AreEqual("FizzBuzz", result);
-            
+            Assert.AreEqual("Fizz", result);
+            Assert.AreEqual("Buzz", result);
+            Assert.AreEqual(randomNummer.ToString(), result);
 
             // Testet kommer att bli grönt då ett NotImplementedException kastas
         }
