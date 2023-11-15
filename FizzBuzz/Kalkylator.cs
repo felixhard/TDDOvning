b3dc0275edbeeefcecd2b18627e63f6ba2b5470b
Max Persson <maxpersson40@gmail.com>
2023-11-15 14:37:12 +0100

    Refactor kod och kortade ner FizzBuzzKalkyKonsol

--- FizzBuzz/Kalkylator.cs
+++ FizzBuzz/Kalkylator.cs
@@ -30,22 +30,7 @@ namespace FizzBuzz
         }
         public static void FizzBuzzKalkylKonsol(int nummer)
         {
-            if (nummer % 3 == 0 && nummer % 5 == 0)
-            {
-                Konsollogg("FizzBuzz");
-            }
-            else if (nummer % 3 == 0)
-            {
-                Konsollogg("Fizz");
-            }
-            else if (nummer % 5 == 0)
-            {
-                Konsollogg("Buzz");
-            }
-            else
-            {
-                Konsollogg(nummer.ToString());
-            }
+                Konsollogg(FizzBuzzKalkyl(nummer));
         }
         public static void FizzBuzzInput(string inputNummer)
         {
